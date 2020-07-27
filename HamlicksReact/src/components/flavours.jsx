ffe39d4e0bfa34b6e0fb40ffbe1da7b50aa059fc
Hamish Harrison <57675214+Noxirus@users.noreply.github.com>
2020-07-27 11:40:06 -0600

    Trying to make GH pages work

--- HamlicksReact/src/components/flavours.jsx
+++ HamlicksReact/src/components/flavours.jsx
@@ -142,7 +142,7 @@ class Flavours extends Component {
               New Flavour
             </Link>
           )}
-          <p className="back">There are no flavours in stock.</p>
+          <p className="back">Getting the flavours from the freezer.</p>
         </div>
       );
 
